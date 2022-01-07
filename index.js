9e1b6d1273e2a5754e0e63bc2de582da3d40fd74
Marcos Cordeiro <marcosc974@gmail.com>
2022-01-07 11:07:11 -0300

    Conectando ao mongodb, criando um model, e inserindo os primeiros dados

--- index.js
+++ index.js
@@ -5,3 +5,43 @@ mongoose.connect("mongodb://localhost/mongodb").then(() => {
 }).catch((erro) => {
     console.log(erro);
 })
+
+
+//Criando um model com mongose
+const UsuariosSchema = mongoose.Schema({
+    nome: {
+        type: String,
+        require: true
+    },
+    sobrenome: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    idade: {
+        type: Number,
+        require: true
+    },
+    pais: {
+        type: String
+    }
+})
+
+mongoose.model('usuarios', UsuariosSchema)
+
+let Marcos = mongoose.model('usuarios')
+
+new Marcos({
+    nome: "Leticia",
+    sobrenome: "Lorrany",
+    email: "Leticia@gmail.com",
+    idade: 9,
+    pais: "Brasil"
+}).save().then(() => {
+    console.log("Usuário cadastrado com sucesso")
+}).catch((erro) => {
+    console.log("Erro ao cadastrar: " + erro)
+})
